48a5e6b5270558361c2ed89b8e5e17fd24404c13
mogoatlhe <iam@ofent.se>
2022-12-01 22:13:20 +0200

    find top three elves total

--- day-1/calorie_count.py
+++ day-1/calorie_count.py
@@ -1,14 +1,29 @@
 
-largest = 0
+largest = [0, 0, 0]
 current = 0
 
-with open("./input.txt") as file:
-    for line in file:
-        if len(line) > 1:
-            current += int(line)
-        else:
-            if current > largest:
-                largest = current
-            current = 0
 
-print(largest)
+def populate_largets(current=0):
+    with open("./input.txt") as file:
+        for line in file:
+            if len(line) > 1:
+                current += int(line)
+            else:
+                for index, value in enumerate(largest):
+                    if current > value:
+                        largest[index] = current
+                        break
+                current = 0
+
+
+def top_three_elves_total():
+    total = 0
+
+    for value in largest:
+        total += value
+
+    print(total)
+
+
+populate_largets(current)
+top_three_elves_total()
