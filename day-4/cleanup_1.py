3fc728c5e18ca70faee8a23301dc76a9e0a236f0
mogoatlhe <iam@ofent.se>
2022-12-04 08:14:03 +0200

    find overlaps in range

--- day-4/cleanup_1.py
+++ day-4/cleanup_1.py
@@ -4,12 +4,25 @@ def get_range_values(line):
     pair.append(line[:line.index("-"):1])
     pair.append(line[line.index("-") + 1:line.index(","):1])
     pair.append(line[line.index(",") + 1:line.rindex("-"):1])
-    pair.append(line[line.rindex("-") + 1:len(line) - 1:1])
+    pair.append(line[line.rindex("-") + 1::1])
 
     return pair
 
 
+def is_overlap(pair):
+    if int(pair[0]) <= int(pair[2]) and int(pair[1]) >= int(pair[3]):
+        return True
+    if int(pair[2]) <= int(pair[0]) and int(pair[3]) >= int(pair[1]):
+        return True
+    return False
+
+
 with open("input.txt") as file:
+    contained_pairs_count = 0
     for line in file:
-        print(get_range_values(line))
-        break
+        pair = get_range_values(line)
+
+        if is_overlap(pair):
+            contained_pairs_count += 1
+
+print(contained_pairs_count)
