f3b415e7e93883420398683ee22a6ca8e556da98
mogoatlhe <u14254922@tuks.co.za>
2024-12-15 02:20:14 +0200

    day 13

---
+++ 2024/13/part2.py
@@ -0,0 +1,36 @@
+import decimal
+
+f = open("main/input.txt", "r")
+data = f.read()
+
+data_as_array = data.split("\n\n")       
+
+def get_xy(value, symbol = "+"):
+    x = value[value.index(f"{symbol}") + 1: value.index(",")]
+    y = value[value.index(f"Y{symbol}") + 2: ]
+
+
+    return [int(x), int(y)]
+
+def is_whole(value):
+    return decimal.Decimal(value).as_tuple().exponent >= 0
+
+tokens = 0
+for arr_item in data_as_array:
+    [a, b, prize] = arr_item.split("\n")
+    [ax, ay] = get_xy(a)
+    [bx, by] = get_xy(b)
+    [prize_x, prize_y] = get_xy(prize, "=")
+    
+    prize_x += 10000000000000
+    prize_y += 10000000000000
+    y = (ay * prize_x - prize_y * ax) / (by * ax - bx * ay)
+    y = y if y > 0 else y * -1
+    x = (prize_x - bx * y) / ax
+    
+    if not is_whole(x) or not is_whole(y):
+        continue
+    
+    tokens += y + x * 3
+
+print(tokens)
